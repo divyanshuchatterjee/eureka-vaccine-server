20d6a3612ceb241e7bc2b10f56a2e33cbdecacff
divyanshuchatterjee <divyanshu007.chatterjee@gmail.com>
2024-01-14 19:49:08 +0530

    first commit

---
+++ src/test/java/com/eurekaServer/vaccinationProject/VaccinationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaServer.vaccinationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
